15820aaace818c6a6692aac659ce1fd9417e3645
lakshaykhokhar2003 <lakshaykhokhar2003@gmail.com>
2026-07-06 16:42:54 +0530

    feat(new): added chat context

--- server.ts
+++ server.ts
@@ -4,6 +4,8 @@ import { apiKey } from "./constants.js";
 
 const client = new Anthropic({ apiKey });
 
+const messages: MessageParam[] = [];
+
 const addUserMessage = (messages: MessageParam[], content: string): void => {
     messages.push({ role: "user", content });
 };
@@ -12,7 +14,7 @@ const addAssistantMessage = (messages: MessageParam[], content: string): void =>
     messages.push({ role: "assistant", content });
 };
 
-const chat = async (messages: MessageParam[]): Promise<void> => {
+const chat = async (messages: MessageParam[]): Promise<string | undefined> => {
     const response = await client.messages.create({
         model: "claude-sonnet-5",
         max_tokens: 1024,
@@ -23,13 +25,18 @@ const chat = async (messages: MessageParam[]): Promise<void> => {
 
     for (const block of response.content) {
         if (block.type === "text") {
-            console.log(`Anthropic: ${block.text}`);
+            // console.log(`Anthropic: ${block.text}`);
+            return block.text;
         }
     }
 };
 
-const messages: MessageParam[] = [];
+addUserMessage(messages, "Define quantum computing in one sentence");
+let answer = await chat(messages);
+addAssistantMessage(messages, answer ?? "No answer received.");
+addUserMessage(messages,"Write another sentence");
+answer = await chat(messages);
+addAssistantMessage(messages, answer ?? "No answer received.");
 
-addUserMessage(messages, "Hello, how are you?");
 
 console.log(messages);
